e4b1334ef1d113f75e9224fe5b07cbc48469b804
Andreyinthesky <and19980316@gmail.com>
2018-08-06 07:40:41 +0000

    fix gunicorn conf, init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
