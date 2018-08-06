88a3edc86e231e89500e3be93d1486486e36d8ad
Andreyinthesky <and19980316@gmail.com>
2018-08-06 13:05:13 +0000

    changed urls.py

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -17,5 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', 'test'),
+    url(r'^login/.*$', 'test', name='login'),
+    url(r'^signup/.*', 'test', name='signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+    url(r'^ask/.*', 'test', name='ask'),
+    url(r'^popular/.*', 'test', name='popular'),
+    url(r'^new/.*', 'test', name='new'),
 ]
