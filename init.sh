9254be3315e1adf698b7bdbd2b072ee1a063be98
Belousov Andrey <36185071+Andreyinthesky@users.noreply.github.com>
2018-08-08 15:03:00 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/etc/ask.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
